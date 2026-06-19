c1b887805e8c27fb403613bfc6dfebc8a048be2a
qlqldks <qlqldks985@gmail.com>
2026-06-19 15:21:15 +0900

    0619 뼈대 2차 수정^p^

---
+++ app.py
@@ -0,0 +1,8 @@
+from flask import Flask, render_template
+from blueprints.member_service_JJH import member_bp
+from blueprints.bank_IGE import bank_bp
+from blueprints.memo_KTK import memo_bp
+from blueprints.oneline_diary_KDM import oneline_diary_bp
+from blueprints.todo_list_SK import todo_bp
+
+
